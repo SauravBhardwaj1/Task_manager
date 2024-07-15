6147e967bc66fd285f03f0ad26f90b1a902e7e5f
Saurav Bhardwaj <sauravbhardwaj836@gmail.com>
2024-07-15 16:04:25 +0530

    Adding modal modalCss editTaskForm css

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import '../styles/Modal.css'
+import { AnimatePresence, motion } from 'framer-motion';
 
-const Modal = () => {
+const Modal = ({ isOpen, onClose, children }) => {
+    if(!isOpen) return null;
+
   return (
-    <div>Modal</div>
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div className="modal-overlay" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} onClick={onClose}>
+          <motion.div className="modal" initial={{ y: -100 }} animate={{ y: 0 }} exit={{ y: 100 }} onClick={(e) => e.stopPropagation()}>
+            <button className="modal-close" onClick={onClose}>×</button>
+            <div className="modal-content">
+              {children}
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
-export default Modal
+export default Modal;
